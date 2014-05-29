e5a63838e508f8c673712a9387c09656508604fc
GrimmOutlook <grimmoutlook87@gmail.com>
2014-05-29 11:18:02 -0400

    Description validations, testing - presence & length

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -53,4 +53,44 @@ describe "Creating todo lists" do
 
   end
 
+  it "displays an error when the todo list has no description" do
+
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+
+    fill_in "Title", with: "Phone Calls"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Phone Calls")
+
+  end
+
+  it "displays an error when the todo list has a description less than 3 characters" do
+
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+
+    fill_in "Title", with: "Phone Calls"
+    fill_in "Description", with: "Jo"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Phone Calls")
+
+  end
+
 end
