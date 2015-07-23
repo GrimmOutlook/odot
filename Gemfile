0c7155cd15f59ba75d2f3d996d495341ef1937ac
GrimmOutlook <grimmoutlook87@gmail.com>
2015-07-22 23:41:42 -0400

    Bundle updated Gemfile.

--- Gemfile
+++ Gemfile
@@ -1,13 +1,13 @@
 source 'https://rubygems.org'
-ruby "2.1.1"
+ruby "2.2.2"
 
-gem 'coffee-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.1.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 1.2'
+gem 'jbuilder', '~> 2.0'
 gem 'jquery-rails'
 gem 'pg'
-gem 'rails', '4.1.0'
-gem 'sass-rails', '~> 4.0.0'
+gem 'rails', '4.2.3'
+gem 'sass-rails', '~> 5.0'
 gem 'turbolinks'
 gem 'uglifier', '>= 1.3.0'
 
@@ -17,10 +17,10 @@ group :doc do
 end
 
 group :development, :test do
-  gem 'rspec-rails', '~> 2.14.2'
+  gem 'rspec-rails', '~> 3.3.3'
 end
 
 group :test do
-  gem 'capybara', '~> 2.2.1'
-  gem 'shoulda-matchers', '~> 2.6.1'
+  gem 'capybara', '>= 1.9.3'
+  gem 'shoulda-matchers', '>= 1.9.2'
 end
--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -3,11 +3,22 @@ require 'spec_helper'
 describe "Viewing todo items" do
   let!(:todo_list) { TodoList.create(title: "Groceries", description: "dinner, chips, salsa") }
 
-  it "displays no items when a todo list is empty" do
+  before do
     visit "/todo_lists"
     within "##{todo_list.id}" do
         click_link "List Items"
     end
-    expect(page).to have_content("TodoItems#index")
+  end
+
+  it "displays the title of the todo list" do
+    within "h1" do
+      expect(page).to have_content(todo_list.title)
+    end
+  end
+
+  it "displays no items when a todo list is empty" do
+    expect(page.all("ul.todo_items li").size).to eq(0)
   end
 end
+
+# To show name of test: rspec --format=documentation spec/features/todo_items/index_spec.rb
